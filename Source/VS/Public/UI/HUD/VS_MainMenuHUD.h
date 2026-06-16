c7993d8045abeec5b64e99b9421aeec3cc0ddb2a
Town <oncwnuHKHVblf46AV1HxEpvaIMWo@git.weixin.qq.com>
2026-06-17 00:36:00 +0800

    加 main menu UI and controller（还要改><）

---
+++ Source/VS/Public/UI/HUD/VS_MainMenuHUD.h
@@ -0,0 +1,17 @@
+// LAvid
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UI/HUD/VS_HUD.h"
+#include "VS_MainMenuHUD.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class VS_API AVS_MainMenuHUD : public AVS_HUD
+{
+	GENERATED_BODY()
+	
+};
